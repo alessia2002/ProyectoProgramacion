f52ce51a4bba36841ec5e68c8178150a987f67b1
carmenc144 <carmen.clementelajo@gmail.com>
2021-04-30 10:45:24 +0200

    cambio2

--- src/gestionpedidos/GestionReparto.java
+++ src/gestionpedidos/GestionReparto.java
@@ -1,5 +1,5 @@
 package gestionpedidos;
-//borrar Borrar Alessia Carmen
+//borrar Borrar Alessia Carmen 2
 
 import gestionpedidos.excepciones.PedidoSinTransporteAsignado;
 import gestionpedidos.mapa.Mapa;
