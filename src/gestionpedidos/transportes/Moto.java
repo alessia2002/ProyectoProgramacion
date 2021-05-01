8a9f02c022aabe960b853f9966fec3d68a692cb8
carmenc144 <carmen.clementelajo@gmail.com>
2021-05-01 11:28:16 +0200

    moto y transporte done

--- src/gestionpedidos/transportes/Moto.java
+++ src/gestionpedidos/transportes/Moto.java
@@ -1,5 +1,17 @@
 package gestionpedidos.transportes;
+import gestionpedidos.mapa.*;
 
-public class Moto {
+public class Moto extends Transporte{
+	private  double eurosPKm = 2;
+	private static final double TARIFA_MIN = 10;
+	public Moto(String codigo, Mapa mapa ) {
+		super(codigo,mapa);
+		
+	}
+	public double coste(String codPosDestino, String codPosOrigen) {
+		return this.getMapa().distancia(codPosDestino, codPosOrigen) * eurosPKm + TARIFA_MIN;
+		
+	}
+	
 
 }
--- src/gestionpedidos/transportes/Transporte.java
+++ src/gestionpedidos/transportes/Transporte.java
@@ -1,5 +1,23 @@
 package gestionpedidos.transportes;
+import gestionpedidos.mapa.*;
 
-public class Transporte {
+public abstract class Transporte {
+	private String codigo;
+	private Mapa mapa;
+	public Transporte(String codigo, Mapa mapa) {
+		this.codigo = codigo;
+		this.mapa =mapa;
+	}
+	public double coste( String codPosDestino ) {
+		return coste(codPosDestino, codigo);
+	}
+	public abstract double coste(String codPosDestino, String codPosOrigen) ;
+	public String getCodigo() {
+		return codigo;
+	}
+	public Mapa getMapa() {
+		return mapa;
+	}
+	
 
 }
